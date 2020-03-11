56883dd83a1d563b9482f26356b85acb56a20348
fozzy-b <55637024+fozzy-b@users.noreply.github.com>
2020-03-11 19:58:30 -0400

    ur mom gay lol

---
+++ Percy Jackson/Assets/Scripts/setSwordPose.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WiimoteApi;
+
+public class setSwordPose : MonoBehaviour
+{
+    public Wiimote remote1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitWiimotes();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FinishedWithWiimotes();
+    }
+
+
+    void InitWiimotes() {
+        WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+        foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+            remote1 = remote;
+        }
+        remote1.SendPlayerLED(false, true, true, false); // LEDs will look like this: [* - - *]
+    }
+
+
+    void FinishedWithWiimotes() {
+        foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+            WiimoteManager.Cleanup(remote);
+        }
+    }
+    
+}
--- README.txt
+++ README.txt
@@ -2,3 +2,9 @@ initial commit
 
 
 BOOF 2020
+
+
+ur mom gay lol
+
+
+Percy Jackson is the Unity Folder
